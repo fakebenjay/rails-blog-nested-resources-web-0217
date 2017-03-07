05a548cd48baf152e60a6ed25ee683a6dae3cd91
fakebenjay <fakebenjay@gmail.com[color]>
2017-03-07 10:33:44 -0500

    Done.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment.save
+      redirect_to @post
+    else
+      flash.now[:danger] = "That's a shitty comment. Say something less stupid."
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :post_id)
+    end
+end
